a3d0693203c6e16f8797ea36bc6805334261fcdb
Evandro <evandrorm89@gmail.com>
2025-12-01 23:17:23 -0300

    passing the chunk of bytes to channels

--- main.go
+++ main.go
@@ -3,34 +3,50 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	t, err := os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer t.Close()
+	defer f.Close()
+
+	receiver := getLinesChannel(f)
+
+	for line := range receiver {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
 
 	current_line := ""
-	for {
-		data := make([]byte, 8)
-		_, err := t.Read(data)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
+	go func() {
+		defer close(ch)
+		for {
+			data := make([]byte, 8)
+			_, err := f.Read(data)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(0)
+			}
+
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				current_line += string(data[:i])
+				data = data[i+1:]
+				ch <- current_line
+				current_line = ""
+			}
+
+			current_line += string(data)
+
 		}
+	}()
 
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			current_line += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", current_line)
-			current_line = ""
-		}
-
-		current_line += string(data)
-
-	}
+	return ch
 }
