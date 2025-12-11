d8e893bb238413e77c55d27593d79419493aebf7
Evandro <evandrorm89@gmail.com>
2025-12-10 21:30:58 -0300

    starting with parsing Request Lines

---
+++ internal/request/request.go
@@ -0,0 +1,24 @@
+package request
+
+import (
+	"io"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+}
+
+func parseRequestLine() {}
