62619202fdddf4d067621f7a97ff6406ad453bab
Evandro Magalhaes <evandrorm89@gmail.com>
2025-12-16 22:14:36 -0300

    feat: finishing RequestFromReader function

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"bytes"
+	"errors"
 	"io"
 )
 
@@ -14,11 +16,50 @@ type RequestLine struct {
 	Method        string
 }
 
+var (
+	ErrBadRequestLine = errors.New("bad request line")
+	ErrInvalidMethod  = errors.New("invalid method")
+	ErrInvalidVersion = errors.New("invalid http version")
+)
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	b, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, err
 	}
+
+	rl, err := parseRequestLine(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: rl,
+	}, nil
 }
 
-func parseRequestLine() {}
+func parseRequestLine(b []byte) (RequestLine, error) {
+	lines := bytes.Split(b, []byte("\r\n"))
+
+	parts := bytes.Split(lines[0], []byte(" "))
+	if len(parts) != 3 {
+		return RequestLine{}, ErrBadRequestLine
+	}
+
+	method := string(parts[0])
+	if method != "GET" && method != "POST" {
+		return RequestLine{}, ErrInvalidMethod
+	}
+	target := string(parts[1])
+	versionParts := bytes.Split(parts[2], []byte("/"))
+	version := string(versionParts[1])
+	if version != "1.1" {
+		return RequestLine{}, ErrInvalidVersion
+	}
+
+	return RequestLine{
+		HttpVersion:   version,
+		RequestTarget: target,
+		Method:        method,
+	}, nil
+}
